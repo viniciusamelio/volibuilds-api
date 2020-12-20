fa4480669a10fdc048ccb335be74bfea5009c8e9
Vinicius Amélio <vinicius.amelio@zoho.com>
2020-12-20 14:06:29 -0300

    Adding cors

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import router from './routes';
+import cors from 'cors';
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use(router);
 app.listen('5000');
